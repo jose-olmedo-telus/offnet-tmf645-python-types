bb2c30ca0812b4bc4822a9b2aca6f4ac7f47cc9d
Jose Olmedo <jose.olmedo@telusinternational.com>
2022-09-14 14:38:59 -0600

    characteristics attribute

--- telus_bulk/models/worker_job/address_processing_job.py
+++ telus_bulk/models/worker_job/address_processing_job.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import Union, Optional, List
 
 from fastapi_camelcase import CamelModel
-
+from telus_bulk.models.tmf_645.characteristic import Characteristic
 from telus_bulk.models.worker_job.place import Place, PlaceAMS
 from telus_bulk.models.worker_job.service_specification import ServiceSpecification
 from telus_bulk.models.product_type import ProductType
@@ -15,6 +15,7 @@ class AddressProcessingJob(CamelModel):
     provide_alternative: bool
     place: Union[PlaceAMS, Place]
     service_specification: ServiceSpecification
+    service_characteristic: Optional[List[Characteristic]] = None
 
     def get_product_type(self):
         default_type = ProductType.nhp
